66e367c767c2bfab90d3200c6db8cd7e6ef904dd
saritasolaskar <saritasolaskar@gmail.com>
2026-04-28 12:23:33 +0530

    .

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-    res.send("Hello DevOps Pipeline Working!");
+    res.send("changed version Hello DevOps Pipeline Working!");
 });
 
 app.listen(3000, () => {
